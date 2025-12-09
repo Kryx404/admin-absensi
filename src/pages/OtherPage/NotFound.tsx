63487275c020a4770e5dcb5df03c2d60ee99bc18
rifkisyhd <rifkisyahada81@gmail.com>
2025-12-09 16:01:04 +0700

    UPDATE: NotFound page text and footer branding

--- src/pages/OtherPage/NotFound.tsx
+++ src/pages/OtherPage/NotFound.tsx
@@ -24,7 +24,7 @@ export default function NotFound() {
           />
 
           <p className="mt-10 mb-6 text-base text-gray-700 dark:text-gray-400 sm:text-lg">
-            We can’t seem to find the page you are looking for!
+           Halaman yang Anda cari tidak ditemukan atau sedang dalam tahap pengembangan.
           </p>
 
           <Link
@@ -36,7 +36,7 @@ export default function NotFound() {
         </div>
         {/* <!-- Footer --> */}
         <p className="absolute text-sm text-center text-gray-500 -translate-x-1/2 bottom-6 left-1/2 dark:text-gray-400">
-          &copy; {new Date().getFullYear()} - TailAdmin
+          &copy; {new Date().getFullYear()} - Kryx404
         </p>
       </div>
     </>
